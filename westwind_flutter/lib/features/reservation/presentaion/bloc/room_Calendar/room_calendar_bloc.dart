eaab189dbadbc8f5a36089c7435a22a5dfd85838
gordon <3225297@gmail.com>
2024-07-08 00:49:03 -0600

    Add Null to Guest model, fixed Guest Page

--- westwind_flutter/lib/features/reservation/presentaion/bloc/room_Calendar/room_calendar_bloc.dart
+++ westwind_flutter/lib/features/reservation/presentaion/bloc/room_Calendar/room_calendar_bloc.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:westwind_client/westwind_client.dart';
-import 'package:westwind_flutter/features/auth/domain/usecases/current_user.dart';
 import 'package:westwind_flutter/features/reservation/domain/repositories/reservation_repository.dart';
 import 'package:westwind_flutter/features/room_transaction/domain/repositories/room_transaction_repository.dart';
 
