7f8ef6a43b1e2ae7694840240c6c7229c657df55
gordon <3225297@gmail.com>
2025-02-28 20:56:01 -0700

    delete total

--- westwind_server/lib/src/endpoints/room_transaction_endpoint.dart
+++ westwind_server/lib/src/endpoints/room_transaction_endpoint.dart
@@ -59,7 +59,7 @@ class RoomTransactionEndpoint extends Endpoint {
                 ItemType.visa,
                 ItemType.laundry,
                 ItemType.deposite,
-              } ) & (t.amount < 0) ) ,
+              } )) ,
         orderByList: (t) => [
               Order(column: t.updateDate, orderDescending: true),
               Order(column: t.transactionDay, orderDescending: true),
